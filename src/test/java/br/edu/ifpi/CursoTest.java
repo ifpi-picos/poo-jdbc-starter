ecf97ddff1f067d2bd355052a85fa29a95c4c45d
Jesiel Viana <jesielviana@proton.me>
2023-11-06 15:09:01 -0300

    comentando testes automatizados

--- src/test/java/br/edu/ifpi/CursoTest.java
+++ src/test/java/br/edu/ifpi/CursoTest.java
@@ -1,35 +1,30 @@
 package br.edu.ifpi;
 
-import static org.junit.Assert.assertTrue;
-import org.junit.Before;
-import org.junit.Test;
-import br.edu.ifpi.enums.StatusCurso;
-
 public class CursoTest {
 
-  Curso cursoPadrao = null;
+  // Curso cursoPadrao = null;
 
 
-  @Before
-  public void setup() {
-    cursoPadrao =
-        new Curso("Padrão", StatusCurso.ABERTO, 80, new Professor("Professor", 1, "89999"));
-  }
+  // @Before
+  // public void setup() {
+  // cursoPadrao =
+  // new Curso("Padrão", StatusCurso.ABERTO, 80, new Professor("Professor", 1, "89999"));
+  // }
 
-  @Test
-  public void cursoAbertoDeveAceitarMatriculaDeAluno() {
-    Aluno aluno = new Aluno("Aluno", 0, "899999");
-    assertTrue(cursoPadrao.getAlunos().size() == 0);
-    cursoPadrao.realizaMatricula(aluno);
-    assertTrue(cursoPadrao.getAlunos().size() == 1);
-  }
+  // @Test
+  // public void cursoAbertoDeveAceitarMatriculaDeAluno() {
+  // Aluno aluno = new Aluno("Aluno", 0, "899999");
+  // assertTrue(cursoPadrao.getAlunos().size() == 0);
+  // cursoPadrao.realizaMatricula(aluno);
+  // assertTrue(cursoPadrao.getAlunos().size() == 1);
+  // }
 
-  @Test
-  public void cursoFechadoDeveRecusarMatriculaDeAluno() {
-    Aluno aluno = new Aluno("Aluno", 0, "899999");
-    cursoPadrao.setStatus(StatusCurso.FECHADO);
-    cursoPadrao.realizaMatricula(aluno);
-    assertTrue(cursoPadrao.getAlunos().size() == 0);
-  }
+  // @Test
+  // public void cursoFechadoDeveRecusarMatriculaDeAluno() {
+  // Aluno aluno = new Aluno("Aluno", 0, "899999");
+  // cursoPadrao.setStatus(StatusCurso.FECHADO);
+  // cursoPadrao.realizaMatricula(aluno);
+  // assertTrue(cursoPadrao.getAlunos().size() == 0);
+  // }
 
 }
